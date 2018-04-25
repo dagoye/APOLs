1f9f138f003488b19e1e6879fead48623fcfd2ad
Deborah Agoye <dagoye2@illinois.edu>
2018-04-25 02:03:38 -0500

    Girl Friends Unite

---
+++ js/apol.js
@@ -0,0 +1,71 @@
+/* global $ */
+// object that gives info
+
+function girls(name, age, location, goal) {
+  this.name = name;
+  this.age = age;
+  this.location = location;
+  this.goal = goal;
+}
+
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+// var Cathy = girls("Cathy", 22, "Chicago, IL", "Meet New People");
+
+// define var name for 10 girls
+
+// Left-arrow clicked (images displayed to decrease by 1)
+$(document).on("click", ".fa-angle-left", function(){
+  var click = 0;
+  click -= 300;
+  $("div#users.container").animate({
+    scrollLeft: "+=" + click
+  }, 500, "linear");
+});
+
+// Right-arrow clicked (images displayed to increase by 1)
+
+$(document).on("click", ".fa-angle-right", function(){
+  var click = 0;
+  click += 300;
+  $("div#users.container").animate({
+    scrollLeft: "+=" + click
+  }, 500, "linear");
+});
+
+// When hovered over image display info
+
+var girlsIMG = document.querySelectorAll("img.girls");
+console.log(girlsIMG);
+
+
+for (var i =0; i<girlsIMG.length; i++) {
+  // var originalIMG = girlsIMG[i].outerHTML;
+  // console.log(originalIMG);
+  girlsIMG[i].addEventListener("mouseover", function(){ 
+  this.classList.toggle("overlay");
+
+    
+  });
+  
+  
+  girlsIMG[i].addEventListener("mouseout", function(){ 
+  this.classList.toggle("overlay");});
+
+}
+// when wone is hovered the others cant be
+
+// function overlay (img){
+//   img.outerHTML = "<div id=\"overlay\">Cathy</div>";
+// }
+
+$(document).on("click", ".fa-chevron-circle-down", function(){
+  
+})
---
+++ js/store.js
@@ -0,0 +1,12 @@
+/* global $ */
+
+// var imgs = document.querySelectorAll(".collections");
+
+
+// imgs.addEventListener('click', function(e){
+//     alert("clicked");
+// });
+
+$(".cols").on("click", function(e){
+    $(this).toggleClass("");
+});
