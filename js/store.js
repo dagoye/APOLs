1810a71fd0b61637013fe45e1461c6a4a9b6ad4e
Deborah Agoye <dagoye2@illinois.edu>
2018-04-25 02:22:21 -0500

    Changes

--- js/store.js
+++ js/store.js
@@ -7,6 +7,26 @@
 //     alert("clicked");
 // });
 
-$(".cols").on("click", function(e){
-    $(this).toggleClass("");
-});
+// $(".cols").on("click", function(e){
+//     console.log("clicked");
+//     $(this).outerHTML="<div id=\"ger\">ge<div>";
+// });
+
+// var imgs = document.getElementsByClassName(".overlay");
+
+// imgs.addEventListener('click', function(e){
+//     alert("clicked");
+// })
+
+var imgs = $("#collections > div div");
+
+// imgs.each(function(){
+//      $(this).fadeTo("fast", 1, function(){
+//          $(this).fadeTo("slow", 0);
+//      });
+// });
+
+//  imgs.hover(function(){
+//         $(this).fadeTo("fast", 1);},
+//     function(){
+//         $(this).fadeTo("fast", 0)});
